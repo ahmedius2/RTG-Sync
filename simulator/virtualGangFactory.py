5ae625c9fd60281e8622c4843b1b89adebf74267
Waqar Ali <wali@ku.edu>
2019-10-16 10:53:44 -0500

    sim: cleanup combination generator

--- simulator/virtualGangFactory.py
+++ simulator/virtualGangFactory.py
@@ -10,59 +10,50 @@ Copyright (C) 2019 KU-CSL
 09-07-2019  Define generator class and its methods
 09-09-2019  Add functionality to estimate gang execution times
 '''
+import math
 
 class CombinationGenerator:
     def __init__ (self, numOfSystemCores):
         self.M = numOfSystemCores
-        self.comboHash = {}
-        self.gangHash = {}
 
         return
 
-    def generate_gang_combinations (self, taskset, debug = True):
-        self.candidateSet = []
+    def generate_gang_combinations (self, taskset, debug = False):
         self.parallelismHash = {}
         self.computeTimeHash = {}
+        self.candidateSet = []
+        self.configHash = {}
+        self.gangHash = {}
 
         for task in taskset:
             self.candidateSet.append (task.name)
             self.parallelismHash [task.name] = task.m
             self.computeTimeHash [task.name] = task.C
 
-        combos = self.__generate_gang_combinations (self.candidateSet)
+        combos = self.__generate_sys_configs (self.candidateSet)
         comboTimes = self.__calc_combination_times (combos)
 
         if debug:
-            self.__dbg_print_gang_combinations (combos)
+            self.__dbg_print_gang_combinations (combos, len (taskset))
 
         return combos, comboTimes
 
-    def __generate_gang_combinations (self, candidateSet, lvl = 0):
-        if not candidateSet:
-            return []
-
+    def __generate_sys_configs (self, candidateSet):
         if len (candidateSet) == 1:
-            return candidateSet [0]
+            return candidateSet
 
         anchorGang = candidateSet [0]
         newCandidateSet = candidateSet [1:]
 
         configSlots = [[anchorGang]]
-        newCandidateSetStr = str (newCandidateSet)
-        if newCandidateSetStr not in self.gangHash:
-            virtualGangs = self.__generate_virtual_gangs (newCandidateSet, lvl)
-            self.gangHash [newCandidateSetStr] = virtualGangs
-        else:
-            virtualGangs = self.gangHash [newCandidateSetStr]
+        virtualGangs = self.__do_memoized_recursion (newCandidateSet,
+                self.gangHash, 'virtual_gangs')
 
-        if self.__has_multiple_items (virtualGangs):
-            for gang in virtualGangs:
-                if self.__has_multiple_items (gang):
-                    configSlots.append ([anchorGang] + gang)
-                else:
-                    configSlots.append ([anchorGang, gang])
-        else:
-            configSlots.append ([anchorGang, virtualGangs])
+        for gang in virtualGangs:
+            if self.__has_multiple_tasks (gang):
+                configSlots.append ([anchorGang] + gang)
+            else:
+                configSlots.append ([anchorGang, gang])
 
         combinations = []
         for config in configSlots:
@@ -71,60 +62,99 @@ class CombinationGenerator:
             if self.__taskset_parallelism (config) > self.M:
                 continue
             else:
-                remainingTasks = sorted (list (set (candidateSet) - set (config)))
-                remainingTasksStr = str (remainingTasks)
-                if remainingTasksStr not in self.comboHash:
-                    subConfigs = self.__generate_gang_combinations (remainingTasks, lvl + 1)
-                    self.comboHash [remainingTasksStr] = subConfigs
-                else:
-                    subConfigs = self.comboHash [remainingTasksStr]
-
-                if subConfigs:
-                    if self.__has_multiple_items (subConfigs):
-                        for cfg in subConfigs:
-                            combinations.append ('%s,%s' % (cfgString, cfg))
-                    else:
-                        combinations.append ('%s,%s' % (cfgString, subConfigs))
-                else:
+                remainingTasks = [t for t in candidateSet if t not in config]
+                if not remainingTasks:
                     combinations.append (cfgString)
+                    continue
+
+                subConfigs = self.__do_memoized_recursion (remainingTasks,
+                        self.configHash, 'sys_configs')
+
+                for cfg in subConfigs:
+                    combinations.append ('%s,%s' % (cfgString, cfg))
 
         return combinations
 
-    def __generate_virtual_gangs (self, candidateSet, topLvl, subLvl = 1):
+    def __generate_virtual_gangs (self, candidateSet):
         if len (candidateSet) == 1:
-            return candidateSet [0]
+            return candidateSet
         else:
             anchorTask = candidateSet [0]
             newCandidateSet = candidateSet [1:]
-
-            newCandidateSetStr = str (newCandidateSet)
-            if newCandidateSetStr not in self.gangHash:
-                subGangs = self.__generate_virtual_gangs (newCandidateSet, topLvl, subLvl+1)
-                self.gangHash [newCandidateSetStr] = subGangs
-            else:
-                subGangs = self.gangHash [newCandidateSetStr]
+            subGangs = self.__do_memoized_recursion (newCandidateSet,
+                    self.gangHash, 'virtual_gangs')
 
             virtualGangs = [anchorTask]
-            if self.__has_multiple_items (subGangs):
-                for partialGang in subGangs:
-                    virtualGangs.append (partialGang)
-                    if self.__has_multiple_items (partialGang):
-                        newGang = [anchorTask] + partialGang
-                        if self.__taskset_parallelism (newGang) <= self.M:
-                            virtualGangs.append (newGang)
-                    else:
-                        newGang = [anchorTask, partialGang]
-                        if self.__taskset_parallelism (newGang) <= self.M:
-                            virtualGangs.append (newGang)
-            else:
-                virtualGangs.append (subGangs)
-                newGang = [anchorTask, subGangs]
-                if self.__taskset_parallelism (newGang) <= self.M:
-                    virtualGangs.append (newGang)
+            for partialGang in subGangs:
+                virtualGangs.append (partialGang)
+                if self.__has_multiple_tasks (partialGang):
+                    newGang = [anchorTask] + partialGang
+                    if self.__taskset_parallelism (newGang) <= self.M:
+                        virtualGangs.append (newGang)
+                else:
+                    newGang = [anchorTask, partialGang]
+                    if self.__taskset_parallelism (newGang) <= self.M:
+                        virtualGangs.append (newGang)
 
         return virtualGangs
 
-    def __has_multiple_items (self, dataStructure):
+    def __do_memoized_recursion (self, candidateSet, lookupHash, function):
+        candidateSetStr = str (candidateSet)
+        if candidateSetStr not in lookupHash:
+            if function == 'sys_configs':
+                solution = self.__generate_sys_configs (candidateSet)
+            elif function == 'virtual_gangs':
+                solution = self.__generate_virtual_gangs (candidateSet)
+            else:
+                raise ValueError, "Unrecognized recursive function: %s" % \
+                            (function)
+
+            lookupHash [candidateSetStr] = solution
+        else:
+            solution = lookupHash [candidateSetStr]
+
+        return solution
+
+    def __calc_max_configs (self, N, M):
+        maxGangs = N
+        numOfConfigs = 0
+        minGangs = int (math.ceil (N / float (M)))
+
+        for k in range (minGangs, maxGangs + 1):
+            numOfConfigs += self.__calc_stirling_number (N, k)
+
+        return numOfConfigs
+
+    def __calc_stirling_number (self, n, k):
+        # Table to store results of subproblems
+        dp = [[0 for i in range(k + 1)]  for j in range(n + 1)]
+
+        # Base cases
+        for i in range (n + 1):
+            dp [i][0] = 0
+
+        for i in range (k + 1):
+            dp [0][k] = 0
+
+        # Fill rest of the entries in
+        # dp [][] in bottom up manner
+        for i in range (1, n + 1):
+            for j in range (1, k + 1):
+                if (j == 1 or i == j):
+                    dp [i][j] = 1
+                else:
+                    dp [i][j] = (j * dp [i - 1][j] + dp [i - 1][j - 1])
+
+        return dp [n][k]
+
+    def __calc_nCr (self, n, r):
+        nFactorial = math.factorial (n)
+        rFactorial = math.factorial (r)
+        nCr = nFactorial / math.factorial (n-r) / rFactorial
+
+        return nCr
+
+    def __has_multiple_tasks (self, dataStructure):
         return isinstance (dataStructure, list)
 
     def __taskset_parallelism (self, taskset):
@@ -176,7 +206,7 @@ class CombinationGenerator:
 
         return slots
 
-    def __dbg_print_gang_combinations (self, configs):
+    def __dbg_print_gang_combinations (self, configs, numOfTasks):
         slots = self.__dbg_configs_to_slots (configs)
         slotIds = sorted (slots.keys ())
 
@@ -192,5 +222,8 @@ class CombinationGenerator:
                 serial += 1
                 netSerial += 1
             print
+
+        calculatedMaxConfigs = self.__calc_max_configs (numOfTasks, self.M)
+        print 'Calculated Max Configs: ', calculatedMaxConfigs
 
         return
