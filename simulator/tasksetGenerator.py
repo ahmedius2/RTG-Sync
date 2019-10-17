91ce59e2903520ba3b498977b62c932957e4b678
Waqar Ali <wali@ku.edu>
2019-10-17 07:58:00 -0500

    sim: new taskset generator

--- simulator/tasksetGenerator.py
+++ simulator/tasksetGenerator.py
@@ -2,9 +2,8 @@ import math, random
 from taskFactory import Task
 
 class Generator:
-    def __init__ (self, numOfCores, period):
+    def __init__ (self, numOfCores):
         self.M = numOfCores
-        self.P = float (period)
 
         return
 
@@ -13,79 +12,66 @@ class Generator:
         utilization = [u + 1 for u in range (self.M)]
 
         for u in utilization:
-            tid = 1
-            taskset [u] = []
-            maxSysVolume = self.P * u
-            maxLightM = minTallM = math.ceil (0.3 * self.M)
+            remUtil = u
+            taskset [u] = {}
 
-            if tasksetType == 'light':
-                maxTallUtil = 0.2 * u
-                maxShortUtil = 0.8 * u
+            while remUtil > 0.001:
+                tid = 1
+                while (1):
+                    p = random.randint (100, 1500)
+                    if p not in taskset [u]:
+                        taskset [u][p] = []
+                        break
 
-                # Generate short tasks
-                shortTasks, tid = self.__generate_tasks (maxShortUtil, 1,
-                        maxLightM, tid)
-                taskset [u] += shortTasks
+                while tid < 10:
+                    if tasksetType == 'mixed':
+                        m = random.randint (1, self.M)
+                    elif tasksetType == 'light':
+                        m = random.randint (1, math.ceil (0.3 * self.M))
+                    elif tasksetType == 'heavy':
+                        m = random.randint (math.ceil (0.3 * self.M), self.M)
+                    else:
+                        raise ValueError, 'Unexpected taskset type: %s' % (tasksetType)
 
-                # Generate tall tasks
-                tallTasks, tid = self.__generate_tasks (maxTallUtil, minTallM,
-                        self.M, tid)
-                taskset [u] += tallTasks
+                    e = random.randint (10,  10 + p / u / m)
+                    v = e * m / float (p)
 
-            elif tasksetType == 'mixed':
-                mixedTasks, tid = self.__generate_tasks (u, 1, self.M, tid)
-                taskset [u] += mixedTasks
+                    if v > remUtil:
+                        e = remUtil * p / float (m)
+                        v = remUtil
 
-            elif tasksetType == 'heavy':
-                maxTallUtil = 0.8 * u
-                maxShortUtil = 0.2 * u
+                    remUtil -= v
+                    # print 'U=%d | P=%d | m=%d | e=%d | v=%f | r=%f' % (u, p, m, e, v, remUtil)
+                    taskset [u][p].append (Task (tid, e, m))
+                    tid += 1
 
-                # Generate short tasks
-                shortTasks, tid = self.__generate_tasks (maxShortUtil, 1,
-                        maxLightM, tid)
-                taskset [u] += shortTasks
-
-                # Generate tall tasks
-                tallTasks, tid = self.__generate_tasks (maxTallUtil, minTallM,
-                        self.M, tid)
-                taskset [u] += tallTasks
+                    if remUtil < 0.001:
+                        self.__verify_taskset (taskset, u)
+                        break
 
         return taskset
 
-    def print_taskset (self, taskset):
-        tidx = 1
+    def __verify_taskset (self, taskset, u):
+        v = 0.0
+        for p in taskset [u]:
+            for t in taskset [u][p]:
+                v += (t.C * t.m / float (p))
 
-        for u in taskset:
-            tasksetVolume = 0
-            print '================ Utilization = %d' % (u)
-            for t in taskset [u]:
-                print '%3d: %s' % (tidx, t)
-                tasksetVolume += t.C * t.m
-                tidx += 1
+        if (u - v) > 0.001:
+            raise ValueError, 'Utilization not met: u=%.3f | v=%.3f' % (u, v)
 
-            tasksetUtilization =  tasksetVolume / self.P
-            print '    Taskset Utilization = %f' % (tasksetUtilization)
         return
 
-    def __generate_tasks (self, maxUtil, minM, maxM, startTid):
-        taskList = []
-        tid = startTid
-        tasksetUtil = 0
+    def print_taskset (self, taskset):
+        tidx = 1
+        for u in taskset:
+            v = 0
+            print
+            for p in taskset [u]:
+                print
+                for t in taskset [u][p]:
+                    v += (t.C * t.m / float (p))
+                    print 'T=%2d: U=%d | P=%4d | V=%.3f | %s' % (tidx, u, p, v, t)
+                    tidx += 1
 
-        while tasksetUtil < maxUtil:
-            remainingUtil = maxUtil - tasksetUtil
-
-            m = random.randint (minM, maxM)
-            e = random.randint (100, 500)
-            u = (m * e) / self.P
-
-            # Make the last task fill the remaining volume
-            if u > remainingUtil:
-                e = (remainingUtil * self.P) / m
-                u = remainingUtil
-
-            taskList.append (Task (tid, e, m))
-            tasksetUtil += u
-            tid += 1
-
-        return taskList, tid
+        return
