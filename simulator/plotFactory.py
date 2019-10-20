b378454732b6d3a030c38861588bb7d8c40f3896
Waqar Ali <wali@ku.edu>
2019-10-20 12:10:47 -0500

    sim: add results with 1000 runs

--- simulator/plotFactory.py
+++ simulator/plotFactory.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from matplotlib import mlab
 import matplotlib.pyplot as plt
@@ -15,19 +16,32 @@ class Plotter:
 
     def create_schedulability_plot (self, resultsHash, workloadType, M):
         fig = plt.figure (figsize = (7, 5))
-        styleHash = {'rtgang': 'r-o', 'rtgsynch_gpc': 'c-^', 'rtgsynch_bfc': 'g-*', 'gangftp': 'm-p', 'threadglobal': 'b-D'}
+        styleHash = {'rtgang'      : {'lbl': 'RT-Gang',       'mrk': 'r-o'},
+                     'gangftp'     : {'lbl': 'Gang-FTP',      'mrk': 'm-p'},
+                     'rtgsynch_gpc': {'lbl': 'RTG-Synch-GPC', 'mrk': 'g-^'},
+                     'rtgsynch_bfc': {'lbl': 'RTG-Synch-BFC', 'mrk': 'b-x'},
+                     'threadglobal': {'lbl': 'Thread-Global', 'mrk': 'c-D'}}
+
+        workloadLabel = {'mixed': 'Mixed',
+                         'light': 'Lightly Parallel',
+                         'heavy': 'Heavily Parallel'}
 
         for policy in resultsHash:
             utils, sched = self.__stratify_data (resultsHash [policy])
-            plt.plot (utils, sched, styleHash [policy], label = policy.capitalize ())
+            plt.plot (utils, sched, styleHash [policy]['mrk'],
+                    label = styleHash [policy]['lbl'])
 
         plt.grid (True)
-        plt.legend (loc = 'upper center', ncol = 2, fontsize = self.legendFz)
-        plt.xlim (1, utils [-1])
         plt.ylim (0, 1.1)
-        plt.xlabel ('Utilization', fontsize = self.labelFz, fontweight = self.labelFw)
-        plt.ylabel ('Schedulability', fontsize = self.labelFz, fontweight = self.labelFw)
-        plt.title ('%s Taskset - M = %d' % (workloadType.capitalize (), M),
+        plt.xlim (1, utils [-1])
+        if workloadType == 'mixed':
+            plt.legend (loc = 'lower left', ncol = 1,
+                    fontsize = self.legendFz)
+        plt.xlabel ('Utilization', fontsize = self.labelFz,
+                fontweight = self.labelFw)
+        plt.ylabel ('Schedulability', fontsize = self.labelFz,
+                fontweight = self.labelFw)
+        plt.title ('%s' % (workloadLabel [workloadType]),
                 fontsize = self.titleFz, fontweight = self.titleFw)
 
         plt.savefig ('%s_c%d.pdf' % (workloadType, M), bbox_inches = 'tight')
