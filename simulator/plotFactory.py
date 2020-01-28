6245d46110d6453f1b3c114571bb50a24ef18778
Waqar Ali <wali@ku.edu>
2020-01-27 23:58:21 -0600

    rtgsync: create sensitivity to tasks per period plots

--- simulator/plotFactory.py
+++ simulator/plotFactory.py
@@ -11,7 +11,53 @@ class Plotter:
         self.titleFw =  'bold'
         self.labelFz =  'x-large'
         self.titleFz =  'xx-large'
-        self.tpp = tasks_per_period
+
+        return
+
+    def create_sensitivity_plot (self, policies, resultsHash, tasksPerPeriod, M):
+        fig, [ax1, ax2, ax3] = plt.subplots (1, 3, sharey = True, sharex = True, figsize = (16, 4))
+        plt.subplots_adjust (wspace = 0.1)
+
+        styleHash = {'rtgang'               : {'lbl': 'RT-Gang',             'mrk': 'r-o'},
+                     'rtgsync-gpc'          : {'lbl': 'RTG-Sync(GPC)',       'mrk': 'g-^'},
+                     'rtgsync-ideal-gpc'    : {'lbl': 'RTG-Sync(GPC-Ideal)', 'mrk': 'g--^'},
+                     'rtgsync-bfc'          : {'lbl': 'RTG-Sync(BFC)',       'mrk': 'b-x'},
+                     'rtgsync-ideal-bfc'    : {'lbl': 'RTG-Sync(BFC-Ideal)', 'mrk': 'b--x'}}
+
+        workloadLabel = {2: 'N = 2',
+                         5: 'N = 5',
+                         10: 'N = 10'}
+
+        legendLocation = {'light': 'upper right',
+                          'mixed': 'lower left',
+                          'heavy': 'lower left'}
+
+        workloadAxes = {2: ax1,
+                        5: ax2,
+                        10: ax3}
+
+        ax1.set_ylabel ("Schedulability", fontsize = self.labelFz, fontweight = self.labelFw)
+        ax2.set_xlabel ("Utilization", fontsize = self.labelFz, fontweight = self.labelFw)
+
+        for t in tasksPerPeriod:
+            ax = workloadAxes [t]
+            for policy in policies:
+                utils, sched = self.__stratify_data (resultsHash [t][policy])
+                ax.plot (utils, sched, styleHash [policy]['mrk'], lw = 2.0,
+                        label = styleHash [policy]['lbl'])
+            ax.set_title (workloadLabel [t], fontsize = self.titleFz, fontweight = self.titleFw, y = 1.12)
+            ax.grid (True)
+
+            for tick in ax.xaxis.get_major_ticks():
+                tick.label1.set_fontsize(15)
+                tick.label1.set_fontweight('bold')
+            for tick in ax.yaxis.get_major_ticks():
+                tick.label1.set_fontsize(15)
+                tick.label1.set_fontweight('bold')
+
+        plt.ylim (-0.05, 1.05)
+        ax3.legend (loc = 'upper left', ncol = 5, fontsize = self.legendFz, handlelength = 4, bbox_to_anchor = (-2.182, 0.22, 3.5, 0.92), mode = {"expand"}, fancybox = True)
+        plt.savefig ('c%d_tpp.pdf' % (M), bbox_inches = 'tight')
 
         return
 
@@ -63,9 +109,7 @@ class Plotter:
 
         plt.ylim (-0.05, 1.05)
         ax3.legend (loc = 'upper left', ncol = 5, fontsize = self.legendFz, handlelength = 4, bbox_to_anchor = (-2.2, 0.22, 3.5, 1), mode = {"expand"}, fancybox = True)
-
-        additional_name = '' if not self.tpp else '_t%s' % (self.tpp)
-        plt.savefig ('c%d%s.pdf' % (M, additional_name), bbox_inches = 'tight')
+        plt.savefig ('c%d.pdf' % (M), bbox_inches = 'tight')
 
         return
 
