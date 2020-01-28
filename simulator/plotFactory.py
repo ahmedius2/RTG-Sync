fce5da82390ff720da792581327e9755938f0030
Waqar Ali <wali@ku.edu>
2020-01-27 22:52:58 -0600

    rtgsync: use subplots to improve resoultion

--- simulator/plotFactory.py
+++ simulator/plotFactory.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 class Plotter:
     def __init__ (self, tasks_per_period):
         self.data = {}
-        self.legendFz = 7.5
+        self.legendFz = 12
         self.labelFw =  'bold'
         self.titleFw =  'bold'
         self.labelFz =  'x-large'
@@ -16,54 +16,56 @@ class Plotter:
         return
 
     def create_schedulability_plot (self, policies, resultsHash, workloadType, M):
-        fig = plt.figure (figsize = (6, 5))
+        fig, [ax1, ax2, ax3] = plt.subplots (1, 3, sharey = True, sharex = True, figsize = (16, 4))
+        plt.subplots_adjust (wspace = 0.1)
+
         styleHash = {'rtgang'               : {'lbl': 'RT-Gang',             'mrk': 'r-o'},
                      'gftp'                 : {'lbl': 'Gang-FTP',            'mrk': 'm-p'},
                      'gftp-ideal'           : {'lbl': 'Gang-FTP (Ideal)',    'mrk': 'm--p'},
                      'threaded'             : {'lbl': 'Threaded',            'mrk': 'c-D'},
                      'threaded-ideal'       : {'lbl': 'Threaded (Ideal)',    'mrk': 'c--D'},
                      'rtgsync-gpc'          : {'lbl': 'RTG-Sync(GPC)',       'mrk': 'g-^'},
-                     'rtgsync-ideal-gpc'    : {'lbl': 'RTG-Sync(Ideal-GPC)', 'mrk': 'g--^'},
+                     'rtgsync-ideal-gpc'    : {'lbl': 'RTG-Sync(GPC-Ideal)', 'mrk': 'g--^'},
                      'rtgsync-bfc'          : {'lbl': 'RTG-Sync(BFC)',       'mrk': 'b-x'},
-                     'rtgsync-ideal-bfc'    : {'lbl': 'RTG-Sync(Ideal-BFC)', 'mrk': 'b--x'}}
+                     'rtgsync-ideal-bfc'    : {'lbl': 'RTG-Sync(BFC-Ideal)', 'mrk': 'b--x'}}
 
-        workloadLabel = {'mixed': 'Mixed',
-                         'light': 'Light',
-                         'heavy': 'Heavy'}
+        workloadLabel = {'light': 'Lightly Parallel',
+                         'mixed': 'Mixed',
+                         'heavy': 'Heavily Parallel'}
 
         legendLocation = {'light': 'upper right',
                           'mixed': 'lower left',
                           'heavy': 'lower left'}
 
-        for policy in policies:
-            utils, sched = self.__stratify_data (resultsHash [policy])
-            plt.plot (utils, sched, styleHash [policy]['mrk'],
-                    label = styleHash [policy]['lbl'])
+        workloadAxes = {'light': ax1,
+                        'mixed': ax2,
+                        'heavy': ax3}
 
-        plt.grid (True)
-        plt.ylim (-0.1, 1.1)
-        plt.xlim (1, utils [-1])
-        if workloadType == 'mixed':
-            plt.legend (loc = legendLocation [workloadType], ncol = 1,
-                    fontsize = self.legendFz, handlelength = 4)
-        plt.xlabel ('Utilization', fontsize = self.labelFz,
+        ax1.set_ylabel ("Schedulability", fontsize = self.labelFz, fontweight = self.labelFw)
+        ax2.set_xlabel ('Utilization', fontsize = self.labelFz,
                 fontweight = self.labelFw)
-        plt.ylabel ('Schedulability', fontsize = self.labelFz,
-                fontweight = self.labelFw)
-        additional_title = '' if not self.tpp else ' | Tasks Per Period = %s' % (self.tpp)
-        plt.title ('%s' % (workloadLabel [workloadType] + additional_title),
-                fontsize = self.titleFz, fontweight = self.titleFw)
 
-        ax = plt.gca ()
-        for tick in ax.xaxis.get_major_ticks():
-            tick.label1.set_fontsize(15)
-            # tick.label1.set_fontweight('bold')
-        for tick in ax.yaxis.get_major_ticks():
-            tick.label1.set_fontsize(15)
-            # tick.label1.set_fontweight('bold')
+        for w in ['heavy', 'mixed', 'light']:
+            ax = workloadAxes [w]
+            for policy in policies:
+                utils, sched = self.__stratify_data (resultsHash [w][policy])
+                ax.plot (utils, sched, styleHash [policy]['mrk'], lw = 2.0,
+                        label = styleHash [policy]['lbl'])
+            ax.set_title (workloadLabel [w], fontsize = self.titleFz, fontweight = self.titleFw, y = 1.2)
+            ax.grid (True)
+
+            for tick in ax.xaxis.get_major_ticks():
+                tick.label1.set_fontsize(15)
+                tick.label1.set_fontweight('bold')
+            for tick in ax.yaxis.get_major_ticks():
+                tick.label1.set_fontsize(15)
+                tick.label1.set_fontweight('bold')
+
+        plt.ylim (-0.05, 1.05)
+        ax3.legend (loc = 'upper left', ncol = 5, fontsize = self.legendFz, handlelength = 4, bbox_to_anchor = (-2.2, 0.22, 3.5, 1), mode = {"expand"}, fancybox = True)
 
         additional_name = '' if not self.tpp else '_t%s' % (self.tpp)
-        plt.savefig ('%s_c%d%s.pdf' % (workloadType, M, additional_name), bbox_inches = 'tight')
+        plt.savefig ('c%d%s.pdf' % (M, additional_name), bbox_inches = 'tight')
 
         return
 
