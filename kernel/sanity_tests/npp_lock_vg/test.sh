4158b873925e3be09d483ebf7243d85c835d9a3f
Waqar Ali <wali@ku.edu>
2020-10-29 11:26:44 -0500

    sanity: stable test for checking NPP lock with virtual gangs

--- kernel/sanity_tests/npp_lock_vg/test.sh
+++ kernel/sanity_tests/npp_lock_vg/test.sh
@@ -14,42 +14,56 @@ if [ ! -f tau_1 ] || [ ! -f tau_2 ]; then
 	return
 fi
 
-echo -e "${GRN}[STATUS] Recording trace without virtual gang${NCL}"
-echo "RT_GANG_LOCK" > /sys/kernel/debug/sched_features
-taskset -c 1 trace-cmd record -e "sched_switch" &> /dev/null &
-trace_pid=$!
-
-sleep 5
-chrt -f 5 taskset -c 3 ./tau_1 -c 20 -p 50 -j 100 &> time.txt & # &> /dev/null &
-chrt -f 6 taskset -c 4 ./tau_2 -c 10 -p 50 -j 100 # &> /dev/null
-sleep 5
-
-kill -s SIGINT ${trace_pid}
-while [ ! -f "trace.dat" ]; do
-	sleep 1
-done
-mv trace.dat without_vgang.trace
-
 echo -e "${GRN}[STATUS] Recording trace with virtual gang${NCL}"
+echo 2 > /sys/kernel/debug/rtgang/debug_level
+echo "RT_GANG_LOCK" > /sys/kernel/debug/sched_features
 taskset -c 2 ${RTG_BUILD_PATH}/rtg_daemon &> /dev/null &
 sleep 1
+taskset -c 2 ${RTG_BUILD_PATH}/rtg_client -c 2 &> /dev/null
 taskset -c 2 ${RTG_BUILD_PATH}/rtg_client -c 2 &> /dev/null
 
 taskset -c 1 trace-cmd record -e "sched_switch" &> /dev/null &
 trace_pid=$!
 
-sleep 5
-chrt -f 5 taskset -c 3 ./tau_1 -c 20 -p 50 -j 100 -v 1001 &> /dev/null &
-chrt -f 6 taskset -c 4 ./tau_2 -c 10 -p 50 -j 100 -v 1001 &> /dev/null
-sleep 5
+sleep 1
+chrt -f 5 taskset -c 3 ./tau_1 -c 5 -p 10 -j 100 -v 1001 &> /dev/null &
+chrt -f 5 taskset -c 4 ./tau_1 -c 5 -p 10 -j 100 -v 1001 &> /dev/null &
+chrt -f 6 taskset -c 4 ./tau_2 -c 1 -p 9  -j 100 -v 1002 &> /dev/null &
+chrt -f 6 taskset -c 5 ./tau_2 -c 1 -p 9  -j 100 -v 1002 &> /dev/null
+sleep 2
 
 kill -s SIGINT ${trace_pid}
 while [ ! -f "trace.dat" ]; do
 	sleep 1
 done
-mv trace.dat with_vgang.trace
-
+mv trace.dat without_npp.trace
 ${RTG_BUILD_PATH}/rtg_client -f 1001 &> /dev/null
+${RTG_BUILD_PATH}/rtg_client -f 1002 &> /dev/null
+
+echo -e "${GRN}[STATUS] Recording trace with virtual gang (NPP)${NCL}"
+sleep 1
+taskset -c 2 ${RTG_BUILD_PATH}/rtg_client -c 2 &> /dev/null
+taskset -c 2 ${RTG_BUILD_PATH}/rtg_client -c 2 &> /dev/null
+
+taskset -c 1 trace-cmd record -e "sched_switch" &> /dev/null &
+trace_pid=$!
+
+sleep 1
+chrt -f 5 taskset -c 3 ./tau_1 -c 5 -p 10 -j 100 -v 1003 -y 1 -l 3 &> /dev/null &
+chrt -f 5 taskset -c 4 ./tau_1 -c 5 -p 10 -j 100 -v 1003 &> /dev/null &
+chrt -f 6 taskset -c 4 ./tau_2 -c 1 -p 9  -j 100 -v 1004 &> /dev/null &
+chrt -f 6 taskset -c 5 ./tau_2 -c 1 -p 9  -j 100 -v 1004 &> /dev/null
+sleep 2
+
+kill -s SIGINT ${trace_pid}
+while [ ! -f "trace.dat" ]; do
+	sleep 1
+done
+mv trace.dat with_npp.trace
+
+${RTG_BUILD_PATH}/rtg_client -f 1003 &> /dev/null
+${RTG_BUILD_PATH}/rtg_client -f 1004 &> /dev/null
 ${RTG_BUILD_PATH}/rtg_client -t &> /dev/null
 echo "NO_RT_GANG_LOCK" > /sys/kernel/debug/sched_features
+echo 0 > /sys/kernel/debug/rtgang/debug_level
 echo -e "${GRN}[STATUS] Experiment Complete!${NCL}"
